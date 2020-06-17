f7fd966a680485fd7ae63ce7e54b82d73d8e4a1e
David Oniani <onianidavid@gmail.com>
2020-06-17 00:52:18 -0500

    nvim-lsp enhancements

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,7 @@
 --
 -- Filename: init.vim
 -- Author:   David Oniani
--- Modified: June 11, 2020
+-- Modified: June 17, 2020
 --
 --  _       _ _     _             
 -- (_)_ __ (_) |_  | |_   _  __ _ 
@@ -19,7 +19,16 @@ local custom_attach = function(_, bufnr)
   require'diagnostic'.on_attach()
 
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-  vim.api.nvim_command('au BufWritePost <buffer> lua vim.lsp.buf.formatting()')
+
+  vim.api.nvim_command('autocmd CursorHold <buffer> lua vim.lsp.util.show_line_diagnostics()')
+  vim.api.nvim_command('autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()')
+
+  local opts = {noremap=true, silent=true}
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ge', ':NextDiagnosticCycle<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gE', ':PrevDiagnosticCycle<CR>', opts)
 end
 
 -- Configure and initialize language servers
--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -1,7 +1,7 @@
 "
 " Filename: init.vim
 " Author:   David Oniani
-" Modified: June 11, 2020
+" Modified: June 17, 2020
 "
 "  _       _ _         _
 " (_)_ __ (_) |___   _(_)_ __ ___
@@ -74,8 +74,6 @@ inoremap <silent><expr> <TAB>
 
 " diagnostic-nvim {{{
 
-let g:diagnostic_enable_virtual_text = 1
-let g:diagnostic_virtual_text_prefix = ' '
 let g:diagnostic_insert_delay = 1
 
 " }}}
