9040a4a9048ffb283f5b5a4f1c6f2939e2ce456b
David Oniani <onianidavid@gmail.com>
2021-01-16 22:35:59 -0600

    Remove redundant config

--- .config/nvim/lua/plugins/statusline.lua
+++ .config/nvim/lua/plugins/statusline.lua
@@ -2,22 +2,6 @@
 local lualine = require("lualine")
 
 lualine.theme = "gruvbox"
-lualine.separator = "|"
-lualine.sections = {
-    lualine_a = { "mode" },
-    lualine_b = { "branch" },
-    lualine_c = { "filename" },
-    lualine_x = { "encoding", "fileformat", "filetype" },
-    lualine_y = { "progress" },
-    lualine_z = { "location" }
-}
-lualine.inactive_sections = {
-    lualine_a = { },
-    lualine_b = { },
-    lualine_c = { "filename" },
-    lualine_x = { "location" },
-    lualine_y = { },
-    lualine_z = { }
-}
 lualine.extensions = { "fzf" }
+
 lualine.status()
