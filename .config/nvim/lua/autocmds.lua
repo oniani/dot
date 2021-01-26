58417181c8984c1de108d729eabaf820f4f5f4c5
David Oniani <onianidavid@gmail.com>
2021-01-26 00:36:47 -0600

    Updates to Neovim setup

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -9,3 +9,15 @@ vim.cmd [[ augroup LuaHighlight ]]
 vim.cmd [[     autocmd! ]]
 vim.cmd [[     autocmd TextYankPost * silent! lua vim.highlight.on_yank { } ]]
 vim.cmd [[ augroup END ]]
+
+-- Transparent background
+vim.cmd [[ augroup TransparentBackground ]]
+vim.cmd [[     autocmd! ]]
+vim.cmd [[     autocmd ColorScheme * hi! Normal ctermbg=none guibg=none ]]
+vim.cmd [[ augroup END ]]
+
+-- Italic comments
+vim.cmd [[ augroup ItalicCommments ]]
+vim.cmd [[     autocmd! ]]
+vim.cmd [[     autocmd ColorScheme * hi! Comment cterm=italic gui=italic ]]
+vim.cmd [[ augroup END ]]
