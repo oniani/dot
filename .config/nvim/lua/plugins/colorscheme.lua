7996b268b1bb33ab99c07c8021d86e35914d4ead
David Oniani <onianidavid@gmail.com>
2021-01-27 05:07:09 -0600

    Updates

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,2 +1,8 @@
 -- Set the colorscheme
 vim.api.nvim_command("colorscheme gruvbox8_hard")
+
+-- Remove the background of `CursorLineNR` attribute
+vim.cmd [[ augroup TransparentCursorLineNRBackground ]]
+vim.cmd [[     autocmd! ]]
+vim.cmd [[     autocmd ColorScheme * hi! CursorLineNR guibg=none ]]
+vim.cmd [[ augroup END ]]
