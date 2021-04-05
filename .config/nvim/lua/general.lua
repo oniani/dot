1b544d5451ca50b0d84319b89ddad9d084988951
David Oniani <onianidavid@gmail.com>
2021-04-04 22:58:20 -0500

    Updated nvim config

--- .config/nvim/lua/general.lua
+++ .config/nvim/lua/general.lua
@@ -1,77 +1,35 @@
--- Use utilities
-local util = require("util")
+-- Access global, buffer-local, and window-local options
+local o = vim.o
+local bo = vim.bo
+local wo = vim.wo
 
--- Set completeopt to have a better completion experience
-util.set("completeopt", "menuone,noinsert,noselect")
+-- Global options
+o.clipboard = o.clipboard .. "unnamedplus"
+o.completeopt = "menuone,noinsert,noselect"
+o.fillchars = "fold: "
+o.hidden = true
+o.ignorecase = true
+o.lazyredraw = true
+o.mouse = "a"
+o.shortmess = o.shortmess .. "c"
+o.smartcase = true
+o.splitbelow = true
+o.splitright = true
+o.termguicolors = true
+o.updatetime = 300
+o.wildmode = "longest:full,full"
+o.writebackup = false
 
--- Avoid showing extra messages when using completion
-util.set("shortmess", "c", "+")
+-- Window-local options
+wo.colorcolumn = "80"
+wo.foldmethod = "marker"
+wo.number = true
+wo.relativenumber = true
+wo.signcolumn = "number"
 
---  Allow for full clipboard
-util.set("clipboard", "unnamedplus", "+")
-
--- Default updatetime leads to noticeable delays and poor user experience
-util.set("updatetime", 300)
-
--- Some servers have issues with backup files
-util.set("nowritebackup")
-
--- Stop generating annoying swap files
-util.set("noswapfile")
-
--- Do not redraw while executing untyped commands (better performance)
-util.set("lazyredraw")
-
--- Possibility to have more than one unsaved buffers
-util.set("hidden")
-
--- Enable line numbers
-util.set("number")
-
--- Enable relative line numbers
-util.set("relativenumber")
-
--- Completion mode
-util.set("wildmode", "longest:full,full")
-
--- Always have `signcolumn` enabled the `number` column
-util.set("signcolumn", "number")
-
--- Set a marker at column 80
-util.set("colorcolumn", 80)
-
--- Horizontal splits below
-util.set("splitbelow")
-
--- Vertical splits right
-util.set("splitright")
-
--- Enable mouse support
-util.set("mouse", "a")
-
--- On pressing <Tab>, insert spaces instead
-util.set("expandtab")
-
--- When indenting with ">", use 4 spaces width
-util.set("shiftwidth", 4)
-
--- Show existing tab with 4 spaces width
-util.set("softtabstop", 4)
-
--- <Tab> counts for 4 spaces
-util.set("tabstop", 4)
-
--- Get rid of fold dashes
-util.set("fillchars", "fold:\\ ")
-
--- Fold based on marker
-util.set("foldmethod", "marker")
-
--- The case of normal letters is ignored
-util.set("ignorecase")
-
--- Case-sensitive if a capital letter is present, case-insensitive otherwise
-util.set("smartcase")
-
--- Enable the true color support
-util.set("termguicolors")
+-- Buffer-local options
+bo.expandtab = true
+bo.shiftwidth = 4
+bo.softtabstop = 4
+bo.swapfile = false
+bo.tabstop = 4
