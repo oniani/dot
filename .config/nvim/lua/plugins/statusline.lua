e6a00d3a94192915a309fff476f1b9ebd35a8631
David Oniani <onianidavid@gmail.com>
2021-04-13 22:21:46 -0500

    Reformat

--- .config/nvim/lua/plugins/statusline.lua
+++ .config/nvim/lua/plugins/statusline.lua
@@ -2,18 +2,18 @@
 require("lualine").setup {
     -- Set the theme
     options = {
-	component_separators = "",
-	icons_enabled = true,
-	section_separators = "",
-	theme = "dracula"
+        component_separators = "",
+        icons_enabled = true,
+        section_separators = "",
+        theme = "dracula"
     },
     -- Additional options
     sections = {
-	lualine_b = { "branch", "diff" },
-	lualine_c = {
-	    { "diagnostics", sources = { "coc" } },
-	    { "filename", full_path = true }
-	}
+        lualine_b = { "branch", "diff" },
+        lualine_c = {
+            { "diagnostics", sources = { "coc" } },
+            { "filename", full_path = true }
+        }
     },
     -- Specify extensions
     extensions = { "fugitive", "fzf" }
