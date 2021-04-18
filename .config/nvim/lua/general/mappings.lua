28a4a3c66274001c6eef0ba8c75456e3d9afff0c
David Oniani <onianidavid@gmail.com>
2021-04-18 10:53:14 -0500

    Updates

--- .config/nvim/lua/general/mappings.lua
+++ .config/nvim/lua/general/mappings.lua
@@ -1,30 +1,32 @@
 -- Access Nvim API
 local api = vim.api
 
+-- Mapping options
+local opts = { noremap = true }
+
 -- Map the leader key to space
 vim.g.mapleader = " "
 
 -- Toggle between buffers
-api.nvim_set_keymap("n", "<Leader><Leader>", "<C-^>", { noremap = true })
+api.nvim_set_keymap("n", "<Leader><Leader>", "<C-^>", opts)
 
 -- Compile or run a program
-api.nvim_set_keymap("n", "<Leader>c", ":!run %<CR>", { noremap = true })
+api.nvim_set_keymap("n", "<Leader>c", ":!run %<CR>", opts)
 
 -- Spell check
-api.nvim_set_keymap("n", "<Leader>s", ":setlocal spell! spelllang=en_us<CR>",
-                    { noremap = true })
+api.nvim_set_keymap("n", "<Leader>s", ":setlocal spell! spl=en_us<CR>", opts)
 
 -- Toggle line wrapping
-api.nvim_set_keymap("n", "<Leader>w", ":set wrap!<CR>", { noremap = true })
+api.nvim_set_keymap("n", "<Leader>w", ":set wrap!<CR>", opts)
 
 -- Window navigation commands (saves a keystroke)
-api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true })
-api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true })
-api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true })
-api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true })
+api.nvim_set_keymap("n", "<C-h>", "<C-w>h", opts)
+api.nvim_set_keymap("n", "<C-j>", "<C-w>j", opts)
+api.nvim_set_keymap("n", "<C-k>", "<C-w>k", opts)
+api.nvim_set_keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize splits
-api.nvim_set_keymap("n", "<Left>",  ":vert resize +5<CR>", { noremap = true })
-api.nvim_set_keymap("n", "<Right>", ":vert resize -5<CR>", { noremap = true })
-api.nvim_set_keymap("n", "<Up>",    ":res +5<CR>",         { noremap = true })
-api.nvim_set_keymap("n", "<Down>",  ":res -5<CR>",         { noremap = true })
+api.nvim_set_keymap("n", "<Down>",  ":res -5<CR>",         opts)
+api.nvim_set_keymap("n", "<Left>",  ":vert resize +5<CR>", opts)
+api.nvim_set_keymap("n", "<Right>", ":vert resize -5<CR>", opts)
+api.nvim_set_keymap("n", "<Up>",    ":res +5<CR>",         opts)
