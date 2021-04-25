94254fa6a21e4d516b59481d48bb6d0a9f479dca
David Oniani <onianidavid@gmail.com>
2021-04-24 19:59:53 -0500

    Custom FZF layout

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -8,3 +8,6 @@ local opts = { noremap = true }
 api.nvim_set_keymap("n", "<Leader>f", ":Files<CR>",  opts)
 api.nvim_set_keymap("n", "<Leader>l", ":BLines<CR>", opts)
 api.nvim_set_keymap("n", "<Leader>r", ":Rg<CR>",     opts)
+
+-- Configure the layout
+vim.g.fzf_layout = { down = "24%" }
