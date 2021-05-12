c0f72136b580c6a3f6fdce47b70c5cf5bf2ffc23
David Oniani <onianidavid@gmail.com>
2021-05-12 05:29:47 -0500

    Update the doc standard

--- .config/nvim/lua/plugins/doge.lua
+++ .config/nvim/lua/plugins/doge.lua
@@ -1,5 +1,9 @@
--- Access Nvim API
+-- Access Nvim API and global editor variables
 local api = vim.api
+local g   = vim.g
+
+-- Set the doc standard
+g.doge_doc_standard_python = "google"
 
 -- Toggle file explorer
 api.nvim_set_keymap("n", "<Leader>d", ":DogeGenerate<CR>", { noremap = true })
