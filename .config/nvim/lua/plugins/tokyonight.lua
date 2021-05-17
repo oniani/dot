3f2b0d0da6a6fd095e593d7e96b8f39c5e83b8c9
David Oniani <onianidavid@gmail.com>
2021-05-17 14:04:23 -0500

    Updates

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -11,14 +11,8 @@ g.tokyonight_sidebars = { "packer", "terminal" }
 g.tokyonight_italic_functions = true
 g.tokyonight_italic_variables = true
 
--- Transparent background
-g.tokyonight_transparent = true
-
 -- Make hints orange and errors bright red
 g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
 
 -- Set the colorscheme
 vim.api.nvim_command("colorscheme tokyonight")
-
--- Make nvim-tree background transparent
-vim.defer_fn(function() vim.cmd("hi! NvimTreeNormal guibg=none") end, 200)
