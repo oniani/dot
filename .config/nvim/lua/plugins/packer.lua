08bf553fef5a116068f8c4052b2716426f09c253
David Oniani <onianidavid@gmail.com>
2022-02-21 16:42:01 -0800

    Updates and cleanup

--- .config/nvim/lua/plugins/packer.lua
+++ .config/nvim/lua/plugins/packer.lua
@@ -6,11 +6,11 @@ local repo = "https://github.com/wbthomason/packer.nvim"
 
 -- Clone packer.nvim
 if vim.fn.empty(vim.fn.glob(path)) > 0 then
-    vim.api.nvim_command(string.format("!git clone --depth 1 %s %s", repo, path))
+    vim.cmd(string.format("!git clone --depth 1 %s %s", repo, path))
 end
 
 -- Required since packer.nvim is in opt pack
-vim.api.nvim_command("packadd packer.nvim")
+vim.cmd("packadd packer.nvim")
 
 -- }}}
 
@@ -69,27 +69,10 @@ return require("packer").startup({
 
         -- Visuals
         use({
-            "RRethy/nvim-base16",
+            "Mofiqul/vscode.nvim",
             config = function()
-                require("base16-colorscheme").setup({
-                    base00 = "#000000",
-                    base01 = "#3c3836",
-                    base02 = "#504945",
-                    base03 = "#665c54",
-                    base04 = "#bdae93",
-                    base05 = "#d5c4a1",
-                    base06 = "#ebdbb2",
-                    base07 = "#fbf1c7",
-                    base08 = "#fb4934",
-                    base09 = "#fe8019",
-                    base0A = "#fabd2f",
-                    base0B = "#b8bb26",
-                    base0C = "#8ec07c",
-                    base0D = "#83a598",
-                    base0E = "#d3869b",
-                    base0F = "#d65d0e",
-                })
-            end,
+                vim.cmd("colorscheme vscode")
+            end
         })
     end,
 })
