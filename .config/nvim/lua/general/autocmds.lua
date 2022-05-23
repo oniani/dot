a65be57c0aa181c55a698cc8306f5e943847b4b3
David Oniani <onianidavid@gmail.com>
2022-05-22 20:39:09 -0700

    Updates

--- .config/nvim/lua/general/autocmds.lua
+++ .config/nvim/lua/general/autocmds.lua
@@ -1,6 +1,5 @@
 -- Defines groups
 local auto_generate = vim.api.nvim_create_augroup("AutoGenerate", { clear = true })
-local lang_specific = vim.api.nvim_create_augroup("LangSpecific", { clear = true })
 local miscellaneous = vim.api.nvim_create_augroup("Miscellaneous", { clear = true })
 
 -- Regenerates Graphviz file on write
@@ -15,17 +14,6 @@ vim.api.nvim_create_autocmd("BufWritePost", {
     command = "silent !latexmk -interaction=nonstopmode -pdf -shell-escape %",
     group = auto_generate,
     pattern = "*.tex",
-})
-
--- Sets the number of paces for a tab in C++
-vim.api.nvim_create_autocmd("Filetype", {
-    callback = function()
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.softtabstop = 2
-        vim.opt_local.tabstop = 2
-    end,
-    group = lang_specific,
-    pattern = "cpp",
 })
 
 -- Equalizes splits when resized
