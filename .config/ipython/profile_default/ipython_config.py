aa50d64bc7c31260ecad1e273dd2efe0b33d89f2
David Oniani <onianidavid@gmail.com>
2022-06-01 09:31:42 -0700

    Updates

---
+++ .config/ipython/profile_default/ipython_config.py
@@ -0,0 +1,13 @@
+from IPython.terminal.prompts import ClassicPrompts
+
+config = get_config()
+
+config.InteractiveShell.color_info = True
+config.InteractiveShell.colors = "Linux"
+config.TerminalIPythonApp.display_banner = False
+config.TerminalInteractiveShell.confirm_exit = False
+config.TerminalInteractiveShell.editing_mode = "vi"
+config.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
+config.TerminalInteractiveShell.highlight_matching_brackets = True
+config.TerminalInteractiveShell.prompts_class = ClassicPrompts
+config.TerminalInteractiveShell.separate_in = ""
