425ed38a33ee475df749cd602860e09c726a8840
David Oniani <onianidavid@gmail.com>
2022-12-26 07:48:36 -0500

    Updates

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,3 +1,101 @@
+-- `on_attach` {{{
+
+local on_attach = function(client, bufnr)
+    local nmap = function(keys, func, desc)
+        if desc then
+            desc = "LSP: " .. desc
+        end
+        vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc, noremap = true })
+    end
+
+    nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+    nmap("T", vim.lsp.buf.signature_help, "Signature Documentation")
+
+    nmap("dl", vim.diagnostic.setloclist, "[D]iagnostic [L]ist")
+    nmap("dn", vim.diagnostic.goto_next, "[D]iagnostic [N]ext")
+    nmap("dp", vim.diagnostic.goto_prev, "[D]iagnostic [P]revious")
+
+    nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+
+    nmap("ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+    nmap("rn", vim.lsp.buf.rename, "[R]e[n]ame")
+
+    vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+        if vim.lsp.buf.format then
+            vim.lsp.buf.format()
+        elseif vim.lsp.buf.formatting then
+            vim.lsp.buf.formatting()
+        end
+    end, { desc = "LSP: Format current buffer" })
+
+    if client.server_capabilities.documentFormattingProvider then
+        local autofmt = vim.api.nvim_create_augroup("AutoFormat", { clear = true })
+        vim.api.nvim_create_autocmd("BufWritePost", {
+            command = ":Format",
+            group = autofmt,
+            pattern = "*",
+        })
+    end
+end
+
+-- }}}
+
+-- Install and set up plugins {{{
+
+local fmt = function(cmd)
+    return { formatCommand = cmd, formatStdin = true }
+end
+
+local runtime_path = vim.split(package.path, ";")
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+local settings = {
+    bashls = {},
+    clangd = {},
+    efm = {
+        rootMarkers = { ".git/" },
+        languages = {
+            lua = { fmt "stylua --column-width 100 --indent-type spaces --call-parentheses None -" },
+            markdown = { fmt "prettier --print-width 100 --stdin-filepath ${INPUT}" },
+            python = { fmt "black --fast --line-length 100 -" },
+        },
+    },
+    gopls = {},
+    pyright = {},
+    rust_analyzer = {},
+    sumneko_lua = {
+        Lua = {
+            runtime = { version = "LuaJIT", path = runtime_path },
+            diagnostics = { globals = { "vim" } },
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false },
+        },
+    },
+}
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
+require("fidget").setup {}
+require("mason").setup {}
+local lspconfig = require "lspconfig"
+local mason_lspconfig = require "mason-lspconfig"
+
+mason_lspconfig.setup { ensure_installed = vim.tbl_keys(settings) }
+mason_lspconfig.setup_handlers {
+    function(server_name)
+        lspconfig[server_name].setup {
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = settings[server_name],
+            init_options = { documentFormatting = true },
+        }
+    end,
+}
+
+-- }}}
+
 -- nvim-cmp {{{
 
 local cmp = require "cmp"
@@ -8,7 +106,7 @@ local has_words_before = function()
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0
         and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s"
-        == nil
+            == nil
 end
 
 cmp.setup {
@@ -91,132 +189,5 @@ cmp.setup.cmdline(":", {
         { name = "cmdline" },
     }),
 })
-
--- }}}
-
--- `on_attach` {{{
-
-local on_attach = function(client, bufnr)
-    local nmap = function(keys, func, desc)
-        if desc then
-            desc = "LSP: " .. desc
-        end
-        vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc, noremap = true })
-    end
-
-    nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-    nmap("T", vim.lsp.buf.signature_help, "Signature Documentation")
-
-    nmap("dl", vim.diagnostic.setloclist, "[D]iagnostic [L]ist")
-    nmap("dn", vim.diagnostic.goto_next, "[D]iagnostic [N]ext")
-    nmap("dp", vim.diagnostic.goto_prev, "[D]iagnostic [P]revious")
-
-    nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-
-    nmap("ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-    nmap("rn", vim.lsp.buf.rename, "[R]e[n]ame")
-
-    vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-        if vim.lsp.buf.format then
-            vim.lsp.buf.format()
-        elseif vim.lsp.buf.formatting then
-            vim.lsp.buf.formatting()
-        end
-    end, { desc = "LSP: Format current buffer" })
-
-    if client.server_capabilities.documentFormattingProvider then
-        local autofmt = vim.api.nvim_create_augroup("AutoFormat", { clear = true })
-        vim.api.nvim_create_autocmd("BufWritePost", {
-            command = ":Format",
-            group = autofmt,
-            pattern = "*",
-        })
-    end
-end
-
--- }}}
-
--- nvim-lspconfig {{{
-
-require("fidget").setup {}
-require("mason").setup {}
-
-local mason_lspconfig = require "mason-lspconfig"
-mason_lspconfig.setup {
-    ensure_installed = {
-        "bashls",
-        "clangd",
-        "efm",
-        "gopls",
-        "pyright",
-        "rust_analyzer",
-        "sumneko_lua",
-    },
-}
-
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-local lspconfig = require "lspconfig"
-for _, server in ipairs(mason_lspconfig.get_installed_servers()) do
-    if server == "clangd" then
-        lspconfig[server].setup {
-            capabilities = capabilities,
-            on_attach = on_attach,
-            fallbackFlags = { "--std=c++20" },
-        }
-    elseif server == "efm" then
-        local fmt = function(cmd)
-            return { formatCommand = cmd, formatStdin = true }
-        end
-        lspconfig[server].setup {
-            capabilities = capabilities,
-            on_attach = on_attach,
-            filetypes = { "lua", "markdown", "python" },
-            init_options = { documentFormatting = true },
-            settings = {
-                rootMarkers = { ".git/" },
-                languages = {
-                    lua = {
-                        fmt "stylua --column-width 100 --indent-type spaces --call-parentheses None -",
-                    },
-                    markdown = {
-                        fmt "prettier --print-width 100 --stdin-filepath ${INPUT}",
-                    },
-                    python = {
-                        fmt "black --fast --line-length 100 -",
-                    },
-                },
-            },
-        }
-    elseif server == "sumneko_lua" then
-        local runtime_path = vim.split(package.path, ";")
-        table.insert(runtime_path, "lua/?.lua")
-        table.insert(runtime_path, "lua/?/init.lua")
-        lspconfig[server].setup {
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = {
-                Lua = {
-                    runtime = {
-                        version = "LuaJIT",
-                        path = runtime_path,
-                    },
-                    diagnostics = {
-                        globals = { "vim" },
-                    },
-                    workspace = {
-                        library = vim.api.nvim_get_runtime_file("", true),
-                        checkThirdParty = false,
-                    },
-                    telemetry = { enable = false },
-                },
-            },
-        }
-    else
-        lspconfig[server].setup {
-            capabilities = capabilities,
-            on_attach = on_attach,
-        }
-    end
-end
 
 -- }}}
--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -3,7 +3,7 @@ require("nvim-treesitter.configs").setup {
         enable = true,
         additional_vim_regex_highlighting = false,
     },
-    indent = { enable = true },
+    indent = { enable = true, disable = { "python" } },
     incremental_selection = {
         enable = true,
         keymaps = {
