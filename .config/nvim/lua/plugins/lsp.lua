59d6a092e7f053b87229991961470ec3003852d3
David Oniani <onianidavid@gmail.com>
2023-02-08 01:14:19 -0500

    nvim-cmp for nvim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,7 +4,16 @@ local M = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
         "j-hui/fidget.nvim",
-        "ms-jpq/coq_nvim",
+
+        -- Autocompletion
+        "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/cmp-path",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "onsails/lspkind-nvim",
     },
 }
 
@@ -32,12 +41,6 @@ local on_attach = function(_, bufnr)
             vim.lsp.buf.formatting()
         end
     end, { desc = "LSP: [F]ormat Current Buffer" })
-
-    vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#16161d", bg = "#16161d" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#16161d" })
-    vim.api.nvim_set_hl(0, "Pmenu", { bg = "#16161d" })
-    vim.api.nvim_set_hl(0, "PmenuSel", { fg = "black", bg = "#8ba8f0" })
-    vim.api.nvim_set_hl(0, "PmenuThumb", { bg = "#8ba8f0" })
 
     vim.diagnostic.config {
         float = { border = "rounded" },
@@ -78,30 +81,108 @@ local server_settings = {
     },
 }
 
-vim.g.coq_settings = {
-    auto_start = "shut-up",
-    clients = { snippets = { warn = {} } },
-    keymap = { jump_to_mark = "<Nop>" },
-}
-
 M.config = function()
     require("fidget").setup {}
     require("mason").setup {}
 
     local lspconfig = require "lspconfig"
     local mason_lspconfig = require "mason-lspconfig"
-    local coq = require "coq"
+
+    local cmp_capabilities = require "cmp_nvim_lsp"
+    local default_capabilities = vim.lsp.protocol.make_client_capabilities()
+    local capabilities = cmp_capabilities.default_capabilities(default_capabilities)
 
     mason_lspconfig.setup { ensure_installed = vim.tbl_keys(server_settings) }
     mason_lspconfig.setup_handlers {
         function(server)
-            lspconfig[server].setup(coq.lsp_ensure_capabilities {
+            lspconfig[server].setup {
+                capabilities = capabilities,
                 on_attach = on_attach,
                 settings = server_settings[server],
                 init_options = { documentFormatting = true },
-            })
+            }
         end,
     }
+
+    local cmp = require "cmp"
+    local lspkind = require "lspkind"
+    local luasnip = require "luasnip"
+
+    cmp.setup {
+        experimental = { ghost_text = true },
+        formatting = {
+            format = lspkind.cmp_format {
+                menu = {
+                    nvim_lsp = "LSP",
+                    luasnip = "LuaSnip",
+                    buffer = "Buffer",
+                    path = "Path",
+                    latex_symbols = "LaTeX",
+                },
+            },
+        },
+        mapping = cmp.mapping.preset.insert {
+            ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-u>"] = cmp.mapping.scroll_docs(4),
+            ["<C-o>"] = cmp.mapping.complete(),
+            ["<C-c>"] = cmp.mapping.close(),
+            ["<C-j>"] = cmp.mapping.select_next_item(),
+            ["<C-k>"] = cmp.mapping.select_prev_item(),
+            ["<CR>"] = cmp.mapping.confirm {
+                behavior = cmp.ConfirmBehavior.Replace,
+                select = true,
+            },
+            ["<Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_next_item()
+                elseif luasnip.expand_or_jumpable() then
+                    luasnip.expand_or_jump()
+                else
+                    fallback()
+                end
+            end, { "i", "s" }),
+            ["<S-Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_prev_item()
+                elseif luasnip.jumpable(-1) then
+                    luasnip.jump(-1)
+                else
+                    fallback()
+                end
+            end, { "i", "s" }),
+        },
+        snippet = {
+            expand = function(args)
+                luasnip.lsp_expand(args.body)
+            end,
+        },
+        sources = {
+            { name = "nvim_lsp" },
+            { name = "luasnip" },
+            { name = "buffer" },
+            { name = "path" },
+        },
+        window = {
+            completion = cmp.config.window.bordered { border = "single" },
+            documentation = cmp.config.window.bordered { border = "single" },
+        },
+    }
+
+    cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+            { name = "buffer" },
+        },
+    })
+
+    cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+            { name = "path" },
+        }, {
+            { name = "cmdline" },
+        }),
+    })
 end
 
 return M
