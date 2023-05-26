fbd041c7ed2a3b70a0be36390ae7c87a5f38fb4b
David Oniani <onianidavid@gmail.com>
2023-05-25 22:36:14 -0400

    Updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,9 +9,17 @@
 --
 
 -- Perf
-vim.loader.enable()
+vim.g.loaded_gzip = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
 vim.g.python3_host_prog = "/home/oniani/.local/share/pyenv/shims/python"
+vim.loader.enable()
 
+-- Setup
 require "autocmd"
 require "keymap"
 require "option"
