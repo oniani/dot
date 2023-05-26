5882c40b32a73791e3920e809dd4dd28772deba5
David Oniani <onianidavid@gmail.com>
2023-05-25 22:22:32 -0400

    Updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,6 +8,10 @@
 -- |_|_| |_|_|\__(_)_|\__,_|\__,_|
 --
 
+-- Perf
+vim.loader.enable()
+vim.g.python3_host_prog = "/home/oniani/.local/share/pyenv/shims/python"
+
 require "autocmd"
 require "keymap"
 require "option"
