f40428ed5345f831bfc154576e7b5813031e58d4
David Oniani <onianidavid@gmail.com>
2023-05-30 16:02:41 -0400

    Updates

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,20 +1,5 @@
 -- Defines groups
-local autogen = vim.api.nvim_create_augroup("AutoGenerate", { clear = true })
 local misc = vim.api.nvim_create_augroup("Miscellaneous", { clear = true })
-
--- Regenerates Graphviz file on write
-vim.api.nvim_create_autocmd("BufWritePost *.dot", {
-    command = "silent !dot -T png % -o %:r.png",
-    group = autogen,
-    pattern = "*.dot",
-})
-
--- Regenerates LaTeX document on write
-vim.api.nvim_create_autocmd("BufWritePost", {
-    command = "silent !latexmk -interaction=nonstopmode -pdf -quiet -shell-escape %",
-    group = autogen,
-    pattern = "*.tex",
-})
 
 -- Equalizes splits when resized
 vim.api.nvim_create_autocmd("VimResized *", {
