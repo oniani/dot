3785fc79d596a266be8e5bdbaa6629bebd1808f2
David Oniani <onianidavid@gmail.com>
2023-06-21 11:34:22 -0400

    Updates

--- .config/wezterm/fonts.lua
+++ .config/wezterm/fonts.lua
@@ -3,6 +3,7 @@ local wezterm = require "wezterm"
 local module = {}
 
 function module.apply_to_config(config)
+    config.adjust_window_size_when_changing_font_size = false
     config.font = wezterm.font "JetBrains Mono"
     config.font_size = 16.0
 end
