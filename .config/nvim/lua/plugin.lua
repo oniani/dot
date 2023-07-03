b833ea9e204646d25ca0406ce7e608f88dd88cbb
David Oniani <onianidavid@gmail.com>
2023-07-03 03:20:47 -0400

    Updates

--- .config/nvim/lua/plugin.lua
+++ .config/nvim/lua/plugin.lua
@@ -141,12 +141,6 @@ local on_attach = function(_, bufnr)
         float = { border = "single" },
         virtual_text = false,
     }
-
-    local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-    for type, icon in pairs(signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, { numhl = hl, text = icon, texthl = hl })
-    end
 end
 
 local f = function(cmd)
