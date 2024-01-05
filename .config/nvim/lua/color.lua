3e31fbd5d454edc81d9fbffcf69a24052ee4aa83
David Oniani <onianidavid@gmail.com>
2024-01-04 22:27:59 -0500

    Updates

--- .config/nvim/lua/color.lua
+++ .config/nvim/lua/color.lua
@@ -1,3 +1,7 @@
+-- Neovim configuration file
+-- by David Oniani <onianidavid@gmail.com>
+-- MIT License
+
 vim.cmd.colorscheme "default"
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -5,4 +9,4 @@ vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "PMenu", { bg = "none" })
 vim.api.nvim_set_hl(0, "PMenuSel", { bg = "NvimLightBlue", fg = "black" })
 vim.api.nvim_set_hl(0, "PMenuThumb", { bg = "NvimLightGrey4" })
-vim.api.nvim_set_hl(0, "Statusline", { bg = "NvimLightBlue", fg="black" })
+vim.api.nvim_set_hl(0, "Statusline", { bg = "NvimLightGrey2", fg="black" })
