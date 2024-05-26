ccb3cdfcf010cfc07950d244438b50e22493bc66
David Oniani <onianidavid@gmail.com>
2024-05-25 21:36:35 -0400

    Updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,16 @@
 -- by David Oniani <onianidavid@gmail.com>
 -- MIT License
 
+-- Perf
+vim.g.did_install_default_menus = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.python3_host_prog = "/home/oniani/.local/share/pyenv/shims/python"
 vim.loader.enable()
 
 require "autocmd"
