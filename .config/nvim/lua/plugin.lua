f294346036a1dc6c096e214295c1306a8dad74ca
David Oniani <onianidavid@gmail.com>
2024-06-01 02:24:28 -0400

    Updates

--- .config/nvim/lua/plugin.lua
+++ .config/nvim/lua/plugin.lua
@@ -264,11 +264,34 @@ cmp.setup.cmdline(":", {
 
 -- }}}
 
--- Colorscheme {{{
+-- Visuals {{{
 
 require("lualine").setup { }
 
 require("kanagawa").setup { transparent = true }
 vim.cmd.colorscheme "kanagawa"
+
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
+vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "none", fg = "#ff9e3b" })
+vim.api.nvim_set_hl(0, "DiagnosticSignError", { bg = "none", fg = "#e82424" })
+vim.api.nvim_set_hl(0, "DiagnosticSignHint", { bg = "none", fg = "#6a9589" })
+vim.api.nvim_set_hl(0, "DiagnosticSignInfo", { bg = "none", fg = "#658594" })
+vim.api.nvim_set_hl(0, "DiagnosticSignWarn", { bg = "none", fg = "#ff9e3b" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none", fg = "#141414" })
+vim.api.nvim_set_hl(0, "FoldColumn", { bg = "none", fg = "#141414" })
+vim.api.nvim_set_hl(0, "Folded", { bg = "none", fg = "#957FB8" })
+vim.api.nvim_set_hl(0, "GitSignsAdd", { bg = "none", fg = "#76946a" })
+vim.api.nvim_set_hl(0, "GitSignsChange", { bg = "none", fg = "#dca561" })
+vim.api.nvim_set_hl(0, "GitSignsChangeDelete", { bg = "none", fg = "#dca561" })
+vim.api.nvim_set_hl(0, "GitSignsDelete", { bg = "none", fg= "#c34043" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#727169" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "black" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "PMenu", { bg = "none", fg = "white" })
+vim.api.nvim_set_hl(0, "PMenuSBar", { bg = "#ff9e3b", fg = "white" })
+vim.api.nvim_set_hl(0, "PMenuSel", { bg = "none", fg = "#8ba8f0" })
+vim.api.nvim_set_hl(0, "PMenuThumb", { bg = "#957fb8", fg = "#141414" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "WildMenu", { bg = "none", fg = "#141414" })
 
 -- }}}
