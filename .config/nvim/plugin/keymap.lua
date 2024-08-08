e53d0bc7fd5cbc310971d3792a34eda03e67c7f6
David Oniani <onianidavid@gmail.com>
2024-08-08 00:42:13 -0400

    Updates

--- .config/nvim/plugin/keymap.lua
+++ .config/nvim/plugin/keymap.lua
@@ -12,6 +12,7 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-t>", "<C-w><S-t>", { desc = "Maximize the current window" })
 
 vim.keymap.set("n", "<Down>", "<C-W>-", { desc = "Shrink window vertically" })
 vim.keymap.set("n", "<Left>", "<c-w>4>", { desc = "Shrink window horizontally" })
