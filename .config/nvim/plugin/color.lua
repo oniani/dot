28fbb6bf3b8512faefc6cf29c47db59a12067fd1
David Oniani <onianidavid@gmail.com>
2024-09-26 00:00:31 -0400

    Updates

--- .config/nvim/plugin/color.lua
+++ .config/nvim/plugin/color.lua
@@ -6,10 +6,6 @@ vim.cmd.colorscheme "default"
 
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
 vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "none", fg = "#ff9e3b" })
-vim.api.nvim_set_hl(0, "DiagnosticSignError", { bg = "none", fg = "#e82424" })
-vim.api.nvim_set_hl(0, "DiagnosticSignHint", { bg = "none", fg = "#6a9589" })
-vim.api.nvim_set_hl(0, "DiagnosticSignInfo", { bg = "none", fg = "#658594" })
-vim.api.nvim_set_hl(0, "DiagnosticSignWarn", { bg = "none", fg = "#ff9e3b" })
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none", fg = "#141414" })
 vim.api.nvim_set_hl(0, "FoldColumn", { bg = "none", fg = "#141414" })
 vim.api.nvim_set_hl(0, "Folded", { bg = "none", fg = "#727169" })
