7a44a89674043e092eae05c8d341cf375851b5b4
David Oniani <onianidavid@gmail.com>
2024-09-26 01:04:34 -0400

    Updates

--- .config/nvim/lua/plugin/fzf.lua
+++ .config/nvim/lua/plugin/fzf.lua
@@ -6,7 +6,7 @@ local fzf = require "fzf-lua"
 
 vim.g.fzf_action = { ["ctrl-s"] = "split", ["ctrl-v"] = "vsplit" }
 
+vim.keymap.set("n", "<C-c>", fzf.git_commits, { desc = "[C]ommits" })
+vim.keymap.set("n", "<C-g>", fzf.live_grep, { desc = "Live Grep" })
 vim.keymap.set("n", "<C-p>", fzf.files, { desc = "Find Files" })
-vim.keymap.set("n", "<Leader-c>", fzf.git_commits, { desc = "[C]ommits" })
-vim.keymap.set("n", "<Leader-g>", fzf.live_grep, { desc = "[G]rep" })
-vim.keymap.set("n", "<Leader-l>", fzf.lines, { desc = "[L]ines" })
+vim.keymap.set("n", "<C-s>", fzf.lines, { desc = "[S]earch Lines" })
