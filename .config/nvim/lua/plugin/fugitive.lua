26640673f0da952903029d466a7754e57c43fd09
David Oniani <onianidavid@gmail.com>
2025-08-02 21:59:36 -0400

    nvim: fugitive.vim use Tab key for toggling diff

--- .config/nvim/lua/plugin/fugitive.lua
+++ .config/nvim/lua/plugin/fugitive.lua
@@ -1,1 +1,8 @@
-
+vim.api.nvim_create_autocmd("FileType", {
+    desc = "Use Tab key for toggling diff in fugitive.vim",
+    group = vim.api.nvim_create_augroup("fugitive-vim-tab-diff", { clear = true }),
+    pattern = "fugitive",
+    callback = function()
+        vim.api.nvim_buf_set_keymap(0, "n", "<Tab>", "=", { noremap = false, silent = true })
+    end,
+})
