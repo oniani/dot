511b3d9dd25af2b6e884eba3b88212bf37a30733
David Oniani <onianidavid@gmail.com>
2025-11-24 00:36:48 -0500

    nvim

--- .config/nvim/after/plugin/nnn.lua
+++ .config/nvim/after/plugin/nnn.lua
@@ -12,7 +12,7 @@ vim.keymap.set({ "n", "t" }, "<C-n>", function()
     vim.cmd "NnnExplorer"
     -- We defer with 100ms delay to ensure nnn vsplit has time to open
     vim.defer_fn(function()
-        vim.cmd "wincmd p"
+        vim.cmd.wincmd({ args = { "p" } })
     end, 100)
 end, { desc = "Toggle nnn in a vertical split" })
 
